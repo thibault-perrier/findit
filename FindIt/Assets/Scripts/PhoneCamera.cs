01f43e3b7ced8f4c3400b566ed5534f0ca8ae388
matcodeew <150682957+matcodeew@users.noreply.github.com>
2024-04-18 12:10:46 +0200

    fix available phone camera

--- FindIt/Assets/Scripts/PhoneCamera.cs
+++ FindIt/Assets/Scripts/PhoneCamera.cs
@@ -9,27 +9,33 @@ public class PhoneCamera : MonoBehaviour
 
     public RawImage background;
     public AspectRatioFitter fit;
+
+    [SerializeField] Image Timer;
+    public float maxTime = 30f;
+    float timeLeft;
     private void Start()
     {
+        Timer.fillAmount = 1;
+        timeLeft = maxTime;
         _defaultBackground = background.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
 
-        if(devices.Length == 0)
+        if (devices.Length == 0)
         {
             Debug.Log("No Camera Detected");
             _camAvailable = false;
             return;
         }
-        for(int i = 0; i < devices.Length; i++)
+        for (int i = 0; i < devices.Length; i++)
         {
-            if(devices[i].isFrontFacing)
+            if (!devices[i].isFrontFacing)
             {
-                 _backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                _backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
 
             }
         }
 
-        if(_backCam == null)
+        if (_backCam == null)
         {
             Debug.Log("Unable to find back camera");
             return;
@@ -43,7 +49,7 @@ public class PhoneCamera : MonoBehaviour
 
     private void Update()
     {
-        if(!_camAvailable)
+        if (!_camAvailable)
         {
             return;
         }
@@ -51,10 +57,16 @@ public class PhoneCamera : MonoBehaviour
         float ratio = (float)_backCam.width / (float)_backCam.height;
         fit.aspectRatio = ratio;
 
-        float scaleY = _backCam.videoVerticallyMirrored ? -1.0f : 1.0f;
-        background.rectTransform.localScale = new Vector3(1, scaleY, 1);
+        //float scaleY = _backCam.videoVerticallyMirrored ? -1.0f : 1.0f;
+        //background.rectTransform.localScale = new Vector3(1, scaleY, 1);
 
         int orient = -_backCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            Timer.fillAmount = timeLeft / maxTime;
+        }
     }
 }
