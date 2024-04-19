f11166f7433c08985b38f5029f05c745c9b7d492
DoriannX <theodor.xav@gmail.com>
2024-04-19 10:47:05 +0200

    Avatar by Sprite

--- FindIt/Assets/Scripts/RandomAvatar.cs
+++ FindIt/Assets/Scripts/RandomAvatar.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 public class RandomAvatar : MonoBehaviour
 {
     [Header("Sprite renderer")]
-    [SerializeField] Image _avatarSprite;
-    [SerializeField] Image _cameraSprite;
+    [SerializeField] GameObject _avatarSprite;
+    [SerializeField] GameObject _cameraSprite;
 
     [Header("List Sprite")]
     [SerializeField] List<Sprite> _cameraSprites = new List<Sprite>();
     [SerializeField] List<Sprite> _avatarSprites = new List<Sprite>();
-
+    [SerializeField] Slider Slider;
+     
     private void Start()
     {
         RandomiseAvatar();
@@ -19,39 +20,18 @@ public class RandomAvatar : MonoBehaviour
 
     public void RandomiseAvatar()
     {
-        _avatarSprite.sprite = _avatarSprites[Random.Range(0, _avatarSprites.Count)];
-        _cameraSprite.sprite = _cameraSprites[Random.Range(0, _cameraSprites.Count)];
+        if(_avatarSprite.TryGetComponent<SpriteRenderer>(out SpriteRenderer avatarSpriteRenderer))
+            avatarSpriteRenderer.sprite = _avatarSprites[Random.Range(0, _avatarSprites.Count)];
+        else if(_avatarSprite.TryGetComponent<Image>(out Image avatarImage))
+            avatarImage.sprite = _avatarSprites[Random.Range(0, _avatarSprites.Count)];
+
+        if (_cameraSprite.TryGetComponent<SpriteRenderer>(out SpriteRenderer cameraSpriteRenderer))
+            cameraSpriteRenderer.sprite = _cameraSprites[Random.Range(0, _cameraSprites.Count)];
+        else if (_cameraSprite.TryGetComponent<Image>(out Image cameraImage))
+            cameraImage.sprite = _cameraSprites[Random.Range(0, _cameraSprites.Count)];
     }
 
-    public void PreviousAvatar()
-    {
-        if (_avatarSprites.IndexOf(_avatarSprite.sprite) > 0)
-            _avatarSprite.sprite = _avatarSprites[_avatarSprites.IndexOf(_avatarSprite.sprite)-1];
-        else
-            _avatarSprite.sprite = _avatarSprites[_avatarSprites.Count - 1];
-    }
+    
 
-    public void NextAvatar()
-    {
-        if (_avatarSprites.IndexOf(_avatarSprite.sprite) < _avatarSprites.Count - 1)
-            _avatarSprite.sprite = _avatarSprites[_avatarSprites.IndexOf(_avatarSprite.sprite) + 1];
-        else
-            _avatarSprite.sprite = _avatarSprites[0];
-    }
-
-    public void PreviousCamera()
-    {
-        if (_cameraSprites.IndexOf(_cameraSprite.sprite) > 0)
-            _cameraSprite.sprite = _cameraSprites[_cameraSprites.IndexOf(_cameraSprite.sprite) - 1];
-        else
-            _cameraSprite.sprite = _cameraSprites[_cameraSprites.Count-1];
-    }
-
-    public void NextCamera()
-    {
-        if (_cameraSprites.IndexOf(_cameraSprite.sprite) < _cameraSprites.Count-1)
-            _cameraSprite.sprite = _cameraSprites[_cameraSprites.IndexOf(_cameraSprite.sprite) + 1];
-        else
-            _cameraSprite.sprite = _cameraSprites[0];
-    }
+    
 }
