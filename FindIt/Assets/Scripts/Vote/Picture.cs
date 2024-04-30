7da9e0a427d2c1605a695d89e6a8fb18d7a9f1ec
matcodeew <150682957+matcodeew@users.noreply.github.com>
2024-04-30 10:03:47 +0200

    Revert "fix scene"
    This reverts commit f0be886b5fbb428377fdb39350af0bbf20b9035a.

--- FindIt/Assets/Scripts/Vote/Picture.cs
+++ FindIt/Assets/Scripts/Vote/Picture.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class Picture : MonoBehaviour
 {
     public static Picture Instance;
-   // public LanManager lanManager;
+    public LanManager lanManager;
     private int nbPlayer;
-   // [SerializeField] private GameObject _prefabsVotes;
-   // [SerializeField] private Transform _transformParent;
+    [SerializeField] private GameObject _prefabsVotes;
+    [SerializeField] private Transform _transformParent;
 
     private void Awake()
     {
@@ -14,14 +14,14 @@ public class Picture : MonoBehaviour
             Instance = this;
     }
 
-    //public void InstantiateButtonVote()
-    //{
-    //    nbPlayer = lanManager.GetAllPlayerInRoom();
-    //    for (int i = 0; i <= nbPlayer; i++)
-    //    {
-    //        Instantiate(_prefabsVotes, Vector3.zero,Quaternion.identity,_transformParent);
-    //    }
-    //}
+    public void InstantiateButtonVote()
+    {
+        nbPlayer = lanManager.GetAllPlayerInRoom();
+        for (int i = 0; i <= nbPlayer; i++)
+        {
+            Instantiate(_prefabsVotes, Vector3.zero,Quaternion.identity,_transformParent);
+        }
+    }
     public void Vote(int nbPlayer)
     {
         foreach(Transform imageVote in transform)
