ff565745defdad1ca92d961ae3a6c6c65848a443
Loh10 <naturellement.toi@gmail.com>
2024-05-03 14:02:20 +0200

    Test Adding Score

--- FindIt/Assets/Scripts/PhotoManager.cs
+++ FindIt/Assets/Scripts/PhotoManager.cs
@@ -9,4 +9,21 @@ public class PhotoManager : MonoBehaviour
     public TextMeshProUGUI debugdefou;
     public string imageKey;
     public List<int> listeScore = new List<int>();
+    public Dictionary<int,int> Point = new Dictionary<int,int>();
+
+    public void AddScoreToAllPicture()
+    {
+        foreach(int i in listeScore)
+        {
+            if(Point.ContainsKey(i+1))
+            {
+                Point[i+1]++; 
+            }
+            else
+            {
+                Point.Add(i+1,1) ;
+            }
+        }
+        print(Point[0]);
+    }
 }
